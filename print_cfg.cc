129393334a0531f3242bc766b4383ea4bdd42d64
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-06-02 16:24:57 -0700

    Iterate over all basic blocks in the function

--- print_cfg.cc
+++ print_cfg.cc
@@ -1,3 +1,4 @@
+#include "llvm/Support/raw_ostream.h"
 #include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
 
@@ -7,7 +8,13 @@ namespace {
   struct PrintCfg : public FunctionPass {
     static char ID;
     PrintCfg() : FunctionPass(ID) {}
-    bool runOnFunction(Function & F __attribute__((unused))) override { return false; }
+    bool runOnFunction(Function & func __attribute__((unused))) override {
+      // Iterate over all basic blocks in the function
+      for (auto it = func.begin(); it != func.end(); it++) {
+        errs() << "Basic block: " << *it << "\n";
+      }
+      return false;
+    }
   };
 }
 
