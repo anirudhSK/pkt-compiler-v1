d097421d8b5c3e3c0f02a4ef92e37b38e511bb06
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-06-06 14:11:48 -0700

    Check for correctness of transpose operation

--- tests/jayhawk_tests.cc
+++ tests/jayhawk_tests.cc
@@ -1,4 +1,41 @@
 #include <iostream>
 #include "gtest/gtest.h"
+#include "graph.cc"
 
-TEST(JayhawkTests, Jayhawk) {}
+TEST(JayhawkTests, Jayhawk) {
+  // Example from Fig. 19.4 b of Appel's book
+  Graph<int> cfg;
+  for (int i = 1; i <= 7; i++) {
+    cfg.add_node(i);
+  }
+
+  // Add edges
+  cfg.add_edge(1, 2);
+  cfg.add_edge(2, 3);
+  cfg.add_edge(2, 4);
+  cfg.add_edge(3, 5);
+  cfg.add_edge(3, 6);
+  cfg.add_edge(5, 7);
+  cfg.add_edge(6, 7);
+  cfg.add_edge(7, 2);
+
+  Graph<int> flipped_cfg;
+  for (int i = 1; i <= 7; i++) {
+    flipped_cfg.add_node(i);
+  }
+
+  // Add edges
+  flipped_cfg.add_edge(2, 1);
+  flipped_cfg.add_edge(3, 2);
+  flipped_cfg.add_edge(4, 2);
+  flipped_cfg.add_edge(5, 3);
+  flipped_cfg.add_edge(6, 3);
+  flipped_cfg.add_edge(7, 5);
+  flipped_cfg.add_edge(7, 6);
+  flipped_cfg.add_edge(2, 7);
+
+  std::cout << "Original CFG \n" << cfg << "\n";
+  std::cout << "Flipped CFG \n" << cfg.transpose() << "\n";
+
+  ASSERT_EQ(cfg.transpose() == flipped_cfg, true);
+}
