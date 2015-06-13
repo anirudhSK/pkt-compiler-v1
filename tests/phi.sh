8bcfd32d36ca2ddcb24bcb3fd66e440e6f5d66ea
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-06-13 15:12:23 -0700

    Improve test, use simplifycfg to eliminate phi nodes
    This is really a workaround because with phi nodes I can't seem to figure
    out how to extract dependencies (issue #7)

--- tests/phi.sh
+++ tests/phi.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
+set -e
 clang -emit-llvm -c phi.c -o test.bc
-opt -load ../.libs/libjayhawk.so -mem2reg -instr_prog_deps test.bc
+opt -load ../.libs/libjayhawk.so -mem2reg -simplifycfg -instr_prog_deps test.bc -o /dev/null > test.output
+diff test.output phi.expected_output
+rm test.output
 rm test.bc
