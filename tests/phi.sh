51c09a9db82a45e7928c82774a8ce5f8f5a47dab
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-06-15 11:13:56 -0700

    Get rid of phi.sh test, it's flaky

--- tests/phi.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-set -e
-clang -emit-llvm -c phi.c -o test.bc
-opt -load ../.libs/libjayhawk.so -mem2reg -simplifycfg -instr_prog_deps test.bc -o /dev/null > test.output
-diff test.output phi.expected_output
-rm test.output
-rm test.bc
