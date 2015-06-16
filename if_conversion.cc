b1423a3e17af7fae65dff3bf93358a14f898043a
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-06-15 20:36:30 -0700

    Denest from anonymous namespace

--- if_conversion.cc
+++ if_conversion.cc
@@ -5,22 +5,21 @@
 
 using namespace llvm;
 
-namespace {
-  struct IfConversion : public FunctionPass {
-    static char ID;
-    IfConversion() : FunctionPass(ID) {}
+struct IfConversion : public FunctionPass {
+ public:
+  static char ID;
+  IfConversion() : FunctionPass(ID) {}
 
-    bool runOnFunction(Function & func) override {
-      errs() << "IfConversion: ";
-      errs().write_escaped(func.getName()) << '\n';
-      for(auto instr = inst_begin(func); instr != inst_end(func); ++instr) {
-        instr->print(errs());
-        errs() << "\n";
-      }
-      return false;
+  bool runOnFunction(Function & func) override {
+    errs() << "IfConversion: ";
+    errs().write_escaped(func.getName()) << '\n';
+    for(auto instr = inst_begin(func); instr != inst_end(func); ++instr) {
+      instr->print(errs());
+      errs() << "\n";
     }
-  };
-}
+    return false;
+  }
+};
 
 char IfConversion::ID = 0;
 static RegisterPass<IfConversion> X("if_conversion", "Convert all branches into guarded statements.", false, false);
