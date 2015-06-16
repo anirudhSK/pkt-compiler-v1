bc30ffba53c3949cd4350f44d3d578a81c815523
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-06-15 20:40:36 -0700

    Break out if_conversion into header and cc files

--- if_conversion.cc
+++ if_conversion.cc
@@ -1,25 +1,15 @@
-#include "llvm/Pass.h"
-#include "llvm/IR/Function.h"
-#include "llvm/Support/raw_ostream.h"
-#include "llvm/IR/InstIterator.h"
-
+#include "if_conversion.h"
 using namespace llvm;
 
-struct IfConversion : public FunctionPass {
- public:
-  static char ID;
-  IfConversion() : FunctionPass(ID) {}
-
-  bool runOnFunction(Function & func) override {
-    errs() << "IfConversion: ";
-    errs().write_escaped(func.getName()) << '\n';
-    for(auto instr = inst_begin(func); instr != inst_end(func); ++instr) {
-      instr->print(errs());
-      errs() << "\n";
-    }
-    return false;
+bool IfConversion::runOnFunction(Function & func) { 
+  errs() << "IfConversion: ";
+  errs().write_escaped(func.getName()) << '\n';
+  for(auto instr = inst_begin(func); instr != inst_end(func); ++instr) {
+    instr->print(errs());
+    errs() << "\n";
   }
-};
+  return false;
+}
 
 char IfConversion::ID = 0;
 static RegisterPass<IfConversion> X("if_conversion", "Convert all branches into guarded statements.", false, false);
---
+++ if_conversion.h
@@ -0,0 +1,16 @@
+#ifndef IF_CONVERSION_H_
+#define IF_CONVERSION_H_
+
+#include "llvm/Pass.h"
+#include "llvm/IR/Function.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/InstIterator.h"
+
+struct IfConversion : public llvm::FunctionPass {
+ public:
+  static char ID;
+  IfConversion() : llvm::FunctionPass(ID) {}
+  bool runOnFunction(llvm::Function & func) override;
+};
+
+#endif  // IF_CONVERSION_H_
