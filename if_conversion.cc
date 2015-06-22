3716af8c3685720e6cddb84bec84eba6f457a8f8
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-06-22 13:22:58 -0700

    Data flow analysis for path conditions

--- if_conversion.cc
+++ if_conversion.cc
@@ -1,12 +1,13 @@
 #include <iostream>
 #include "llvm/IR/Instructions.h"
+#include "llvm/ADT/PostOrderIterator.h"
 #include "llvm/Analysis/CFG.h"
 #include "utility_functions.h"
 #include "if_conversion.h"
 
 using namespace llvm;
 
-bool IfConversion::runOnFunction(Function & func) { 
+bool IfConversion::runOnFunction(Function & func) {
   errs() << "IfConversion: ";
   errs().write_escaped(func.getName()) << '\n';
   for(auto instr = inst_begin(func); instr != inst_end(func); ++instr) {
@@ -16,22 +17,28 @@ bool IfConversion::runOnFunction(Function & func) {
 
   // Check that there are no back edges
   SmallVector<std::pair<const BasicBlock *, const BasicBlock *>, 100> result;
-  llvm::FindFunctionBackedges(func, result);
+  FindFunctionBackedges(func, result);
   if (result.size() > 0) {
     throw std::invalid_argument("Supplied function body has a loop\n");
   }
 
-  // Now start at the first block and propagate path conditions
-  transfer_fn(& func.getEntryBlock(), "");
+  // Now propagate path conditions in topologically sorted order
+  // Reverse Post Order Traversal gets us topological sort
+  ReversePostOrderTraversal<const Function*> rpot(&func);
+  for (ReversePostOrderTraversal<const Function *>::rpo_iterator it = rpot.begin(); it != rpot.end(); ++it) {
+    const auto * current_bb = *it;
+
+    /// Get "out" vectors of all predecessors
+    std::vector<BranchConditions> out_preds;
+    for (const_pred_iterator pi = pred_begin(current_bb); pi != pred_end(current_bb); ++pi) {
+      out_preds.emplace_back(out_states_.at(*pi));
+    }
+
+    in_states_[current_bb]  = join_fn(current_bb, out_preds);
+    out_states_[current_bb] = transfer_fn(current_bb, in_states_.at(current_bb));
+  }
 
   return false;
-}
-
-void IfConversion::bb_walk(const BasicBlock * bb, const Value * incoming_condition __attribute__ ((unused))) {
-  for (unsigned int i = 0; i < bb->getTerminator()->getNumSuccessors(); i++) {
-    bb_walk(bb->getTerminator()->getSuccessor(i));
-  }
-  errs() << "Finished bb_walk for bb " << *bb << "\n";
 }
 
 IfConversion::BranchConditions IfConversion::transfer_fn(const BasicBlock * bb, const BoolExpr & in) const {
@@ -56,22 +63,22 @@ IfConversion::BranchConditions IfConversion::transfer_fn(const BasicBlock * bb, 
       return {single_outgoing_edge};
     }
   } else if (isa<ReturnInst>(bb->getTerminator())) {
-    assert(false);
+    std::cout << "Within a return instruction\n";
     return BranchConditions();
   } else {
-    assert(false);
+    throw std::logic_error("Some other kind of branch\n");
     return BranchConditions();
   }
 }
 
 IfConversion::BoolExpr IfConversion::join_fn(const BasicBlock * bb,
                                              const std::vector<BranchConditions> & outp) const {
-  BoolExpr in;
+  BoolExpr in = "true";
   for (const auto & br_conds : outp) {
     for (const auto & br_edge : br_conds) {
       // If edge points to this basic block, add its condition to a list of or operands
       if (br_edge.first == bb) {
-        in += "(" + br_edge.second + ") or ";
+        in += "or (" + br_edge.second + ")";
       }
     }
   }
--- if_conversion.h
+++ if_conversion.h
@@ -3,6 +3,7 @@
 
 #include <utility>
 #include <string>
+#include <map>
 #include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
 #include "llvm/Support/raw_ostream.h"
@@ -32,13 +33,21 @@ struct IfConversion : public llvm::FunctionPass {
   void bb_walk(const llvm::BasicBlock * bb, const llvm::Value * incoming_condition = {});
 
   /// Transfer function for path condition propagation
+  /// Arguments are the current basic block bb and
+  /// the Boolean Expression coming into this basic block
   BranchConditions transfer_fn(const llvm::BasicBlock * bb, const BoolExpr & in) const;
 
   /// Join function for path condition propagation
   /// Arguments are current basic block bb
-  /// and a vector of outgoing BranchConditions one for each predecessor
+  /// and a vector of outgoing BranchConditions,
+  /// one for each predecessor
   BoolExpr join_fn(const llvm::BasicBlock * bb, const std::vector<BranchConditions> & outp) const;
 
+  /// Out states for each block
+  std::map<const llvm::BasicBlock *, BranchConditions> out_states_ = {};
+
+  /// In states for each block
+  std::map<const llvm::BasicBlock *, BoolExpr> in_states_ = {};
 };
 
 #endif  // IF_CONVERSION_H_
