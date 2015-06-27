2a68922d213e8849b2486bdff9c2fa5bf815569b
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-06-27 13:39:56 -0700

    Improve command to compile clang.
    Clang's really painful, and just can't fit within automake without
    substantial effort. The killer is the start-group, end-group required
    to resolve circular dependencies. This just doesn't work with LDFLAGS
    http://lists.gnu.org/archive/html/automake/2006-11/msg00018.html
    Punting on this for now.

--- clang.sh
+++ clang.sh
@@ -1,4 +1,4 @@
-g++ --std=c++11 -isystem /usr/lib/llvm-3.5/include/ $1 \
+g++ --std=c++11 -isystem /usr/lib/llvm-3.5/include/ $@ \
 	-L /usr/lib/llvm-3.5/lib/  \
 	-Wl,--start-group \
 	-lclangAST \
